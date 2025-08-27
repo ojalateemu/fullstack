31f13306752dca431f277126a6741da8e6b748ef
Teemu <teemuojala15@gmail.com>
2025-08-27 13:41:38 +0300

    Production build of frontend, combining frontend and backend

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -7,6 +7,7 @@ const app = express()
 
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.static('dist'))
 
 let persons = [
     {
